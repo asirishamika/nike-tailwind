5d803ccfd34e8f3177e3d2e6e45cd7e5b9e01054
Asiri shamika <107676071+asirishamika@users.noreply.github.com>
2024-01-23 21:47:33 +0530

    hero section finalized

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const PopularProducts = () => {
-  return (
-    <div>PopularProducts</div>
-  )
-}
+  return <div>PopularProducts</div>;
+};
 
-export default PopularProducts
+export default PopularProducts;
