c5a5757e470901ad485ef585e1628ba781225a5c
Asiri shamika <107676071+asirishamika@users.noreply.github.com>
2024-01-23 17:54:36 +0530

    Navigation bar design

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import {Hero, CustomerReview,Footer,PopularProducts,Services,SpecialOffer,Subscribe,SuperQuality} from "./sections";
+import Nav from "./components/Nav";
 const App =()=>(
   <main className="relative">
-    {/* <Nav/> */}
+     <Nav/> 
     <section className="xl:padding-1 wide:padding-r padding-b">
     <Hero/>
     </section>
